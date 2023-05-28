fff3d6a0e60a7dc46c8f293b93d98ae4c7148ca5
Kacper Adamczak <kacper.adamczak@gmail.com>
2023-05-28 23:24:50 +0200

    dodanie endpointu edit.car.ts

---
+++ src/backend/routes/car/edit.car.ts
@@ -0,0 +1,46 @@
+import { Response, Request } from "express";
+import { param } from "express-validator";
+import { StatusCodes } from "http-status-codes";
+import { prisma } from "../../database";
+import { TRoute } from "../types";
+import { handleRequest, TCustomError } from "../../utils/request.utils";
+import { authorize } from "../../utils/middleware.utils";
+
+const editCarValidators = [authorize, param("id").isInt({ min: 1 })];
+
+const editCarHandler = async (req: Request, res: Response) =>
+    handleRequest({
+        req,
+        res,
+        responseSuccessStatus: StatusCodes.OK,
+        responseFailStatus: StatusCodes.BAD_REQUEST,
+        execute: async () => {
+            const { brand, model, color, productionYear, price } = req.body;
+            const carId = Number(req.params.id);
+            const existingCar = await prisma.car.findUnique({
+                where: { id: carId },
+            });
+
+            if (!existingCar) {
+                throw {
+                    isCustomError: true,
+                    status: StatusCodes.NOT_FOUND,
+                    message: "Nie znaleziono samochodu o podanym id.",
+                } as TCustomError;
+            }
+
+            const updatedCar = await prisma.car.update({
+                where: { id: carId },
+                data: { brand, model, color, productionYear, price },
+            });
+
+            return updatedCar;
+        },
+    });
+
+export default {
+    method: "put",
+    path: "/api/car/add",
+    validators: editCarValidators,
+    handler: editCarHandler,
+} as TRoute;
