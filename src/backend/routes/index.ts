ce0c16b0905a64f1ce83c2c5ebd63e1c0f4cac9f
Kacper Adamczak <kacper.adamczak@gmail.com>
2023-05-28 15:58:16 +0200

    dodanie endpointu rejestracji

--- src/backend/routes/index.ts
+++ src/backend/routes/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import getStatus from "./status/get.status";
+import loginUser from "./user/login.user";
+import registerUser from "./user/register.user";
 
 const router = express.Router();
 
-const apiRoutes = [getStatus];
+const apiRoutes = [getStatus, loginUser, registerUser];
 
 apiRoutes.forEach((route) =>
     router[route.method](route.path, route.validators, route.handler),
---
+++ src/backend/routes/user/register.user.ts
@@ -0,0 +1,59 @@
+import { Response, Request } from "express";
+import { body } from "express-validator";
+import { StatusCodes } from "http-status-codes";
+import { prisma } from "../../database";
+import { TRoute } from "../types";
+import { handleRequest, TCustomError } from "../../utils/request.utils";
+import { createHash } from "../../utils/hash.utils";
+import { createToken } from "../../utils/jwt.utils";
+
+const SALT = (process.env.PASSWORD_SALT as string) ?? "XYZ";
+const SECRET = (process.env.PASSWORD_SECRET as string) ?? "XYZ";
+
+const registrationValidators = [
+    body("name").notEmpty(),
+    body("email").isEmail(),
+    body("password").notEmpty(),
+];
+
+const registrationHandler = async (req: Request, res: Response) =>
+    handleRequest({
+        req,
+        res,
+        responseSuccessStatus: StatusCodes.OK,
+        responseFailStatus: StatusCodes.BAD_REQUEST,
+        execute: async () => {
+            const { name, surname, email, password } = req.body;
+            const passwordHash = createHash(password, SALT);
+            const existingUser = await prisma.employee.findFirst({
+                where: { email },
+            });
+
+            if (existingUser) {
+                throw {
+                    status: StatusCodes.CONFLICT,
+                    message: "Użytkownik o podanym adresie email już istnieje",
+                    isCustomError: true,
+                } as TCustomError;
+            }
+
+            const newUser = await prisma.employee.create({
+                data: {
+                    name,
+                    surname,
+                    email,
+                    password: passwordHash,
+                },
+            });
+            return {
+                token: createToken(newUser, SECRET, "7d"),
+            };
+        },
+    });
+
+export default {
+    method: "post",
+    path: "/api/register",
+    validators: registrationValidators,
+    handler: registrationHandler,
+} as TRoute;
