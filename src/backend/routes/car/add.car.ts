504fd6b946a8e55392106cd34135ee5a6327d3c8
Kacper Adamczak <kacper.adamczak@gmail.com>
2023-05-28 22:28:41 +0200

    endpoint dodaj auto

--- src/backend/routes/car/add.car.ts
+++ src/backend/routes/car/add.car.ts
@@ -1,16 +1,13 @@
 import { Response, Request } from "express";
 import { body } from "express-validator";
-import { StatusCodes, ReasonPhrases } from "http-status-codes";
+import { StatusCodes } from "http-status-codes";
 import { prisma } from "../../database";
 import { TRoute } from "../types";
-import { handleRequest, TCustomError } from "../../utils/request.utils";
-import { createHash } from "../../utils/hash.utils";
-import { createToken } from "../../utils/jwt.utils";
-
-const SALT = (process.env.PASSWORD_SALT as string) ?? "XYZ";
-const SECRET = (process.env.PASSWORD_SECRET as string) ?? "XYZ";
+import { handleRequest } from "../../utils/request.utils";
+import { authorize } from "../../utils/middleware.utils";
 
 const addCarValidators = [
+    authorize,
     body("brand").not().isEmpty(),
     body("model").not().isEmpty(),
     body("color").not().isEmpty(),
@@ -18,8 +15,32 @@ const addCarValidators = [
     body("price").not().isEmpty(),
 ];
 
+const addCarHandler = async (req: Request, res: Response) =>
+    handleRequest({
+        req,
+        res,
+        responseSuccessStatus: StatusCodes.OK,
+        responseFailStatus: StatusCodes.UNAUTHORIZED,
+        execute: async () => {
+            const { brand, model, color, productionYear, price } = req.body;
+            const car = await prisma.car.create({
+                data: {
+                    brand,
+                    model,
+                    color,
+                    productionYear,
+                    price,
+                },
+            });
+            return {
+                car,
+            };
+        },
+    });
+
 export default {
     method: "post",
     path: "/api/car/add",
     validators: addCarValidators,
-};
+    handler: addCarHandler,
+} as TRoute;
