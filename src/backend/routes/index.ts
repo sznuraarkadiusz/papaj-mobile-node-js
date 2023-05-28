75ac4b9308f74fc6c0ff0317346745e104835ef8
Kacper Adamczak <kacper.adamczak@gmail.com>
2023-05-28 22:30:20 +0200

    dodanie endpointu dodaj auto

--- src/backend/routes/index.ts
+++ src/backend/routes/index.ts
@@ -2,10 +2,11 @@ import express from "express";
 import getStatus from "./status/get.status";
 import loginUser from "./user/login.user";
 import registerUser from "./user/register.user";
+import addCar from "./car/add.car";
 
 const router = express.Router();
 
-const apiRoutes = [getStatus, loginUser, registerUser];
+const apiRoutes = [getStatus, loginUser, registerUser, addCar];
 
 apiRoutes.forEach((route) =>
     router[route.method](route.path, route.validators, route.handler),
