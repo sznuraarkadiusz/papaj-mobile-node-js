6ac3945bfedbe5dc2cb28dd43922a92fa382c511
Kacper Adamczak <kacper.adamczak@gmail.com>
2023-05-31 00:14:46 +0200

    rozwój endpointu dodawania rezerwacji

--- src/backend/routes/reservation/reservation.add.ts
+++ src/backend/routes/reservation/reservation.add.ts
@@ -3,12 +3,9 @@ import { body } from "express-validator";
 import { StatusCodes } from "http-status-codes";
 import { prisma } from "../../database";
 import { TRoute } from "../types";
-import { handleRequest } from "../../utils/request.utils";
-import { authorize, isAdminMiddleware } from "../../utils/middleware.utils";
+import { handleRequest, TCustomError } from "../../utils/request.utils";
 
 const addReservationValidators = [
-    authorize,
-    isAdminMiddleware,
     body("reservationDate").not().isEmpty(),
     body("reservationReturnDate").not().isEmpty(),
     body("userId").not().isEmpty(),
@@ -32,6 +29,23 @@ const addReservationHandler = async (req: Request, res: Response) =>
                     reservationReturnDate,
                 },
             });
+            const carAvailibility = await prisma.car.findFirst({
+                where: { id: carId },
+            });
+
+            if (!carAvailibility?.isAvailable) {
+                throw {
+                    status: StatusCodes.BAD_REQUEST,
+                    message: "Wybrany samochód jest niedostępny",
+                    isCustomError: true,
+                } as TCustomError;
+            } else {
+                await prisma.car.update({
+                    where: { id: carId },
+                    data: { isAvailable: false },
+                });
+            }
+
             return {
                 reservation,
             };
