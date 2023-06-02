db30190c687b689899bd413a28be88d82b413fdd
Kacper Adamczak <kacper.adamczak@gmail.com>
2023-06-03 01:50:55 +0200

    dodanie typu auto info

---
+++ src/backend/routes/car/types.car.ts
@@ -0,0 +1,10 @@
+export type TCarInfo = {
+    id: number;
+    brand: string;
+    model: string;
+    year: number;
+    color: string;
+    price: number;
+    isAvailable: boolean;
+    averageRate: number;
+};
